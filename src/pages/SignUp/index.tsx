60789ef5674daa9ba1cc21f921f2fafe9deeeeea
Hewerton Oliveira <hewerton.comp@gmail.com>
2020-12-09 21:13:12 -0300

    Added form handler lib

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,43 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import * as S from './styles';
 
-const SignIn: React.FC = () => (
-  <S.Container>
-    <S.Background />
-    <S.Content>
-      <img src={logoImg} alt="GoBarber Logo" />
+const SignIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-      <form action="">
-        <h1>Faça seu cadastro</h1>
+  return (
+    <S.Container>
+      <S.Background />
+      <S.Content>
+        <img src={logoImg} alt="GoBarber Logo" />
 
-        <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
-        <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="Senha"
-        />
-        <Button>Cadastrar</Button>
-      </form>
-      <a href="new">
-        <FiArrowLeft />
-        Voltar para login
-      </a>
-    </S.Content>
-  </S.Container>
-);
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
+          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="new">
+          <FiArrowLeft />
+          Voltar para login
+        </a>
+      </S.Content>
+    </S.Container>
+  );
+};
 
 export default SignIn;
